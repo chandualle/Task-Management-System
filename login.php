032d9ec63e0276abf34f83560d52f9a207036671
Alle Chandrashekar <151987574+chandualle@users.noreply.github.com>
2024-06-30 07:53:51 +0530

    Update login.php

--- login.php
+++ login.php
@@ -45,16 +45,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <h2>User Login</h2>
-    <form action="login.php" method="post">
-        <label for="username">Username:</label><br>
-        <input type="text" id="username" name="username" required><br><br>
+    <div class="container">
+        <h2>User Login</h2>
+        <form action="login.php" method="post">
+            <label for="username">Username:</label><br>
+            <input type="text" id="username" name="username" required><br><br>
 
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password" required><br><br>
+            <label for="password">Password:</label><br>
+            <input type="password" id="password" name="password" required><br><br>
 
-        <input type="submit" value="Login">
-    </form>
-    <p>Don't have an account? <a href="register.php">Register here</a>.</p>
+            <input type="submit" value="Login">
+        </form>
+        <p>Don't have an account? <a href="register.php">Register here</a>.</p>
+    </div>
 </body>
 </html>
